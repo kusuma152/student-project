ade83990c8545b0ab9e41655a59387e6c8dc89e1
kusuma152 <57591571+kusuma152@users.noreply.github.com>
2021-11-27 02:22:52 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,8 @@
+CREATE TABLE 'project01'.'student'(
+'student_no' INT(3) NOT NULL,
+'student_name' TEXT(30) NOT NULL,
+'student_dob' DATE NOT NULL,
+'student_doj' DATE NOT NULL,
+PRIMARY KEY('student_no'));
+
+SELECT * FROM project01.student;
